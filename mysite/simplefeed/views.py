565b50e03613f4a18a7a77a947d073d5ed89fdc3
Utkarsh <utkarsh39@hotmail.com>
2016-11-13 20:41:04 +0530

    Web parsing added

--- mysite/simplefeed/views.py
+++ mysite/simplefeed/views.py
@@ -3,6 +3,24 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .models import Target
 from django.template import loader
 import json
+import urllib, re
+from bs4 import BeautifulSoup
+
+def visible(element):
+    if element.parent.name in ['style', 'script', '[document]', 'head', 'title','spanid','button']:
+        return False
+    elif re.match('<!--.*-->', unicode(element)):
+        return False
+    return True
+
+def parsehtml(url):
+    html = urllib.urlopen(url).read()
+    soup = BeautifulSoup(html, "html.parser")
+    texts = soup.findAll(text=True)
+    visible_texts = filter(visible, texts)
+    s = ''.join(visible_texts)
+    s = ''.join(s.split())
+    return s
 
 def index(request):
     target_list = Target.objects.order_by('url').all()
@@ -21,7 +39,8 @@ def addform(request):
 
 
 def add(request):
-    q = Target(url = request.POST['url'], category = request.POST['category'], content = "")
+
+    q = Target(url = request.POST['url'], category = request.POST['category'], content = parsehtml(request.POST['url']))
     q.save()
     return HttpResponseRedirect('/feed/')
 
@@ -37,10 +56,9 @@ def update(request, target_id):
     q = Target.objects.get(pk = target_id)
     q.url = request.POST['url']
     q.category = request.POST['category']
-    q.content = ""
+    q.content = parsehtml(request.POST['url'])
     q.save()
     return HttpResponseRedirect("/feed/")
-
 
 def delete(request, target_id):
     q = Target.objects.get(pk = target_id)
